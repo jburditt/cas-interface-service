3705ff4b987208920228f15d806c9f4947830683
Jebb Burditt <jebb.burditt@gmail.com>
2025-04-30 18:33:54 -0700

    find supplier by name controller service

--- Model/ArgumentExtensions.cs
+++ Model/ArgumentExtensions.cs
@@ -2,25 +2,34 @@
 
 public static class ArgumentExtensions
 {
-    public static void ThrowIfNull<T>(this T obj, string message = null)
+    public static T ThrowIfNull<T>(this T obj, string message = null)
     {
         if (obj == null)
         {
             throw new ArgumentNullException(message ?? $"Argument was null.");
         }
+        return obj;
     }
 
-    public static void ThrowIfNullOrEmpty(this string str, string message = null)
+    public static string ThrowIfNullOrEmpty(this string str, string message = null)
     {
         if (string.IsNullOrEmpty(str))
         {
             throw new ArgumentNullException(message ?? $"Argument was null or empty.");
         }
+        return str;
     }
 
     public static async Task ThrowIfNotSuccessful(this Task<HttpStatusCode> statusCode)
     {
         if (await statusCode != HttpStatusCode.OK)
             throw new HttpRequestException($"Request failed with status code: {statusCode}");
+    }
+
+    public static string ThrowIfNotMinLength(this string str, int length)
+    {
+        if (string.IsNullOrEmpty(str) || str.Length < length)
+            throw new ArgumentException($"String length must be at least {length} characters.");
+        return str;
     }
 }
