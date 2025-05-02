1120754fdab471583912c90b599cf2d7bd8212f5
Jebb Burditt <jebb.burditt@gmail.com>
2025-05-02 13:47:13 -0700

    add get payment service

--- api/Invoice/InvoiceController.cs
+++ api/Invoice/InvoiceController.cs
@@ -22,7 +22,6 @@ public class InvoiceController : Controller
     public async Task<IActionResult> Generate([FromBody] Invoice invoice)
     {
         (var result, var statusCode) = await _casHttpClient.CreateInvoice(invoice);
-
         return StatusCode((int)statusCode, new JsonResult(result).Value);
     }
 
@@ -30,7 +29,13 @@ public class InvoiceController : Controller
     public async Task<IActionResult> Search(string invoiceNumber, string supplierNumber, string supplierSiteCode)
     {
         (var result, var statusCode) = await _casHttpClient.GetInvoice(invoiceNumber, supplierNumber, supplierSiteCode);
+        return StatusCode((int)statusCode, new JsonResult(result).Value);
+    }
 
+    [HttpGet("payment/{paymentNumber}/{payGroup}")]
+    public async Task<IActionResult> GetPayment(string paymentNumber, string payGroup)
+    {
+        (var result, var statusCode) = await _casHttpClient.GetPayment(paymentNumber, payGroup);
         return StatusCode((int)statusCode, new JsonResult(result).Value);
     }
 }
